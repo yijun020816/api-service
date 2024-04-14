a4589cd4eee1dd16bf6342e4058d54dc89915382
易均 <2841159677@qq.com>
2024-04-14 20:24:51 +0800

    更新baseurl

--- utils/url.ts
+++ utils/url.ts
@@ -1,4 +1,4 @@
 export function Url(path = '') {
-  const baseUrl = process.env.NODE_ENV === 'development' ? `http://localhost` : 'https://api.yijun.fun'
+  const baseUrl = process.env.NODE_ENV === 'development' ? `http://localhost` : 'https://service.yijun.fun'
   return baseUrl + path
 }
