6798873299fd93d6218bd68452de1e44cdf63433
凹凸曼 <2841159677@qq.com>
2024-04-15 11:13:27 +0800

    新增获取当前城市今日天气接口

--- server/api/weather.get.ts
+++ server/api/weather.get.ts
@@ -1,14 +1,6 @@
-interface Query {
-  city: string
-}
-
-export default defineCachedEventHandler(async (event) => {
-  const { city = '' } = getQuery<Query>(event)
-
-  const id = getCityCode(city)
-
+export default defineCachedEventHandler(async (_event) => {
   // http://api.help.bj.cn/
-  const data = await (await fetch(`https://api.help.bj.cn/apis/weather/?id=${id}`)).json()
+  const data = await (await fetch('https://api.oioweb.cn/api/weather/GetWeather')).json()
 
   return data
 })
