0393f5f7665f199713c4b44f8ce460c7a2b26133
凹凸曼 <2841159677@qq.com>
2024-04-15 11:12:11 +0800

    删除假数据api  原因-不利于调试

--- server/api/fake/[...all].ts
+++
@@ -1,8 +0,0 @@
-export default defineEventHandler(async (event) => {
-  event.context.cache = { ttl: TimeUnit.second * 3 }
-
-  const params = event.context.params
-  const data = await (await fetch(`https://jsonplaceholder.typicode.com/${params.all ?? ''}`)).json()
-
-  return data
-})
