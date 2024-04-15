c048d7084eec50dbec7153e4a7a0cd86570f4d59
凹凸曼 <2841159677@qq.com>
2024-04-15 13:16:26 +0800

    预渲染路由 加快访问速度

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,17 @@
+import fs from 'node:fs'
+
+function prerenderRoutes() { // 预渲染路由 加快访问速度
+  // 读取根目录下的content下的docs文件夹下的所有json文件
+  const files = fs.readdirSync('./content/docs')
+  // 过滤出json文件
+  const jsonFiles = files.filter(file => file.endsWith('.json'))
+  const routes = jsonFiles.map((file) => {
+    return `/docs/${file.replace('.json', '')}`
+  })
+  routes.push('/')
+  return routes as string[]
+}
+
 export default defineNuxtConfig({
   runtimeConfig: {
     public: {
@@ -53,6 +67,9 @@ export default defineNuxtConfig({
         password: 'ZCpKCgKaBrkWifGdgCQGBl4Fy260CqlF',
         host: 'redis-11121.c11.us-east-1-3.ec2.cloud.redislabs.com',
       },
+    },
+    prerender: {
+      routes: prerenderRoutes(),
     },
   },
   experimental: {
